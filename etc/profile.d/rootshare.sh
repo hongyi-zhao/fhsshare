f94f629bfe0f2127014659b2d654c26ae49e5588
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-01-30 12:02:22 +0800

    HOMESHARE=$ROOTSHARE/home/share

--- etc/profile.d/rootshare.sh
+++ etc/profile.d/rootshare.sh
@@ -174,7 +174,7 @@ while IFS= read -r uuid; do
     #HOMESHARE_GIT=$ROOTSHARE/homeshare.git
  
     # This directory is for holding public data:
-    HOMESHARE=$ROOTSHARE/homeshare
+    HOMESHARE=$ROOTSHARE/home/share
 
     # Third party applications, say, intel's tools, are intalled in this directory for sharing:
     OPTSHARE=$ROOTSHARE/opt
