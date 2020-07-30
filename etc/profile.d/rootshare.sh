a370c6ddbac2bc74c699c75a76e44dac786f5caa
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-07-30 10:21:25 +0800

    [[ "$script_realpath" =~ ^(.*)[.](bash|sh)$ ]] && script_repo_dir="${BASH_REMATCH[1]}" || return
    modified:   etc/profile.d/rootshare.sh

--- etc/profile.d/rootshare.sh
+++ etc/profile.d/rootshare.sh
@@ -2,69 +2,8 @@
 # Obtain the canonicalized absolute dirname where the script resides.
 # Both readlink and realpath can do the trick.
 
-# In the following method, the $script_dirname is equivalent to $topdir:
 script_realpath="$(realpath -e -- "${BASH_SOURCE[0]}")"
-
-topdir=$(
-cd -P -- "$(dirname -- "$script_realpath")" &&
-pwd -P
-) 
-
-if [[ "$script_realpath" =~ ^(.*)/(.*)$ ]]; then
-  script_dirname="${BASH_REMATCH[1]}"
-  script_name="${BASH_REMATCH[2]}"
-  #echo script_dirname="$script_dirname"
-  #echo script_name="$script_name"
-  # . not appeared in script_name at all.
-  if [[ "$script_name"  =~ ^([^.]*)$ ]]; then
-    script_basename="$script_name"
-    #echo script_basename="$script_basename"
-  else
-    # . appeared in script_name. 
-    # As far as filename is concerned, when . is used as the last character, it doesn't have any spefical meaning.
-    # Including . as the beginning character.
-    if [[ "$script_name"  =~ ^([.].*)$ ]]; then
-      script_extname="$script_name"
-      #echo script_extname="$script_extname"
-    # Including . but not as the beginning/trailing character.
-    elif [[ "$script_name"  =~ ^([^.].*)[.]([^.]+)$  ]]; then
-      script_basename="${BASH_REMATCH[1]}"
-      script_extname="${BASH_REMATCH[2]}"
-      #echo script_basename="$script_basename"
-      #echo script_extname="$script_extname"
-    fi
-  fi
-fi
-
-#https://unix.stackexchange.com/questions/18886/why-is-while-ifs-read-used-so-often-instead-of-ifs-while-read
-
-# software/anti-gfw/not-used/vpngate-relative/ecmp-vpngate/script/ovpn-traverse.sh
-# man find:
-# -printf format
-# %f     File's name with any leading directories removed (only the last element).
-# %h     Leading directories of file's name (all but the last element).  
-# If the file name contains  no  slashes
-#             (since it is in the current directory) the %h specifier expands to `.'.       
-# %H     Starting-point under which file was found.  
-# %p     File's name.
-# %P     File's name with the name of the starting-point under which it was found removed.
-
-
-
-#https://superuser.com/questions/731425/bash-detect-execute-vs-source-in-a-script
-#https://stackoverflow.com/questions/2683279/how-to-detect-if-a-script-is-being-sourced
-# Only triggering the cd command logic when script is not being sourced.
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-  if [[ $script_extname == "sh" ]]; then
-    if [ -d "$topdir/$script_basename" ]; then  
-      ncore=$(sudo dmidecode -t 4 | grep 'Core Enabled:' | awk '{a+=$NF}END{ print a }')
-      cd $topdir/$script_basename
-    else
-      cd $topdir  
-    fi
-  fi
-fi
-
+[[ "$script_realpath" =~ ^(.*)[.](bash|sh)$ ]] && script_repo_dir="${BASH_REMATCH[1]}" || return
 
 
 # The idea
@@ -183,13 +122,13 @@ if [ "$( id -u )" -ne 0 ] && [ -d "$ROOTSHARE_ORIGIN_DIR" ] && [ -d "$HOMESHARE_
     export XDG_DATA_DIRS=$HOME/.local/share:$XDG_DATA_DIRS
   fi
 
-  if ! grep -Eq '/usr/local/share[/]?(:|$)' <<< $XDG_DATA_DIRS; then
-    export XDG_DATA_DIRS=/usr/local/share:$XDG_DATA_DIRS
-  fi
+  #if ! grep -Eq '/usr/local/share[/]?(:|$)' <<< $XDG_DATA_DIRS; then
+  #  export XDG_DATA_DIRS=/usr/local/share:$XDG_DATA_DIRS
+  #fi
 
-  if ! grep -Eq '/usr/share[/]?(:|$)' <<< $XDG_DATA_DIRS; then
-    export XDG_DATA_DIRS=/usr/share:$XDG_DATA_DIRS
-  fi
+  #if ! grep -Eq '/usr/share[/]?(:|$)' <<< $XDG_DATA_DIRS; then
+  #  export XDG_DATA_DIRS=/usr/share:$XDG_DATA_DIRS
+  #fi
 
   # attach the stuff found on $HOMESHARE_WORK_TREE/ at $HOME/:
 
@@ -287,10 +226,6 @@ fi
   
   
   
-
-
-
-
 # ref：
 # https://unix.stackexchange.com/questions/348321/purpose-of-the-autostart-scripts-directory
 #https://specifications.freedesktop.org/autostart-spec/autostart-spec-latest.html
