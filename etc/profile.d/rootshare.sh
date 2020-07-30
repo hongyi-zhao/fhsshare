f05840dacd105eeb417758dcd26b9a2efa51be0b
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-07-30 13:34:01 +0800

    [[ "$script_realpath" =~ ^(.*)[.](bash|sh)$ ]] && repo_dir="${BASH_REMATCH[1]}" || return
    modified:   etc/profile.d/rootshare.sh

--- etc/profile.d/rootshare.sh
+++ etc/profile.d/rootshare.sh
@@ -3,7 +3,7 @@
 # Both readlink and realpath can do the trick.
 
 script_realpath="$(realpath -e -- "${BASH_SOURCE[0]}")"
-[[ "$script_realpath" =~ ^(.*)[.](bash|sh)$ ]] && script_repo_dir="${BASH_REMATCH[1]}" || return
+[[ "$script_realpath" =~ ^(.*)[.](bash|sh)$ ]] && repo_dir="${BASH_REMATCH[1]}" || return
 
 
 # The idea
