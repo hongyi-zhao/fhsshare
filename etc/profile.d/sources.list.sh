df128dc98c5ea1cd8d1805a9fe05adb171d1ad62
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-08-27 21:09:09 +0800

    new file:   etc/profile.d/sources.list.sh

---
+++ etc/profile.d/sources.list.sh
@@ -0,0 +1,53 @@
+#!/use/bin/env bash
+
+codename=$(lsb_release -sc)
+sources_list=/etc/apt/sources.list11
+
+if [ ! -e $sources_list ] || ! grep -q -m1 '^deb https://mirrors.tuna.tsinghua.edu.cn/' $sources_list; then
+  if lsb_release -i | grep -qi Debian; then
+	cat <<-EOF | sudo tee $sources_list >/dev/null
+		#http://mirrors.ustc.edu.cn/repogen/
+		#https://mirrors.tuna.tsinghua.edu.cn/help/debian/
+	
+		#如果遇到无法拉取 https 源的情况，请先使用 http 源并安装：
+		#$ sudo apt install apt-transport-https
+	
+		deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename main contrib non-free
+		deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename main contrib non-free
+		deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-updates main contrib non-free
+		deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-updates main contrib non-free
+		#deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-backports main contrib non-free
+		# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-backports main contrib non-free
+		deb https://mirrors.tuna.tsinghua.edu.cn/debian-security $codename/updates main contrib non-free
+		deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security $codename/updates main contrib non-free
+	EOF
+  elif lsb_release -i | grep -qi Ubuntu; then
+	cat <<-EOF | sudo tee $sources_list >/dev/null
+		#http://mirrors.ustc.edu.cn/repogen/
+		#https://mirrors.tuna.tsinghua.edu.cn/help/debian/
+		
+		#如果遇到无法拉取 https 源的情况，请先使用 http 源并安装：
+		#$ sudo apt install apt-transport-https
+
+		
+		deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename main restricted universe multiverse
+		deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename main restricted universe multiverse
+		
+		deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-security main restricted universe multiverse
+		deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-security main restricted universe multiverse
+		
+		deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-updates main restricted universe multiverse
+		deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-updates main restricted universe multiverse
+		
+		deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-backports main restricted universe multiverse
+		deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-backports main restricted universe multiverse
+		
+		## Not recommended
+		# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-proposed main restricted universe multiverse
+		# deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-proposed main restricted universe multiverse 
+	EOF
+  fi	
+fi
+
+
+
