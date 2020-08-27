548e5f13976baaaaffa52e5da20a07e1abda67f4
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-08-28 07:30:55 +0800

    https://unix.stackexchange.com/questions/76481/cant-indent-heredoc-to-match-code-blocks-indentation
    modified:   etc/profile.d/sources.list.sh

--- etc/profile.d/sources.list.sh
+++ etc/profile.d/sources.list.sh
@@ -3,48 +3,49 @@
 codename=$(lsb_release -sc)
 sources_list=/etc/apt/sources.list11
 
+
 if [ ! -e $sources_list ] || ! grep -q -m1 '^deb https://mirrors.tuna.tsinghua.edu.cn/' $sources_list; then
   if lsb_release -i | grep -qi Debian; then
-	cat <<-EOF | sudo tee $sources_list >/dev/null
-		#http://mirrors.ustc.edu.cn/repogen/
-		#https://mirrors.tuna.tsinghua.edu.cn/help/debian/
-	
-		#如果遇到无法拉取 https 源的情况，请先使用 http 源并安装：
-		#$ sudo apt install apt-transport-https
-	
-		deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename main contrib non-free
-		deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename main contrib non-free
-		deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-updates main contrib non-free
-		deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-updates main contrib non-free
-		#deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-backports main contrib non-free
-		# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-backports main contrib non-free
-		deb https://mirrors.tuna.tsinghua.edu.cn/debian-security $codename/updates main contrib non-free
-		deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security $codename/updates main contrib non-free
+        #https://unix.stackexchange.com/questions/76481/cant-indent-heredoc-to-match-code-blocks-indentation
+	cat <<-EOF | gawk 'NR==1 && match($0, /^ +/){n=RLENGTH} {print substr($0, n+1)}' | sudo tee $sources_list >/dev/null
+        #http://mirrors.ustc.edu.cn/repogen/
+        #https://mirrors.tuna.tsinghua.edu.cn/help/debian/
+
+        #如果遇到无法拉取 https 源的情况，请先使用 http 源并安装：
+        #$ sudo apt install apt-transport-https
+
+        deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename main contrib non-free
+        deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename main contrib non-free
+        deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-updates main contrib non-free
+        deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-updates main contrib non-free
+        deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-backports main contrib non-free
+        # deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-backports main contrib non-free
+        deb https://mirrors.tuna.tsinghua.edu.cn/debian-security $codename/updates main contrib non-free
+        deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security $codename/updates main contrib non-free
 	EOF
   elif lsb_release -i | grep -qi Ubuntu; then
-	cat <<-EOF | sudo tee $sources_list >/dev/null
-		#http://mirrors.ustc.edu.cn/repogen/
-		#https://mirrors.tuna.tsinghua.edu.cn/help/debian/
-		
-		#如果遇到无法拉取 https 源的情况，请先使用 http 源并安装：
-		#$ sudo apt install apt-transport-https
+	cat <<-EOF | gawk 'NR==1 && match($0, /^ +/){n=RLENGTH} {print substr($0, n+1)}' | sudo tee $sources_list >/dev/null
+        #http://mirrors.ustc.edu.cn/repogen/
+        #https://mirrors.tuna.tsinghua.edu.cn/help/debian/
 
-		
-		deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename main restricted universe multiverse
-		deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename main restricted universe multiverse
-		
-		deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-security main restricted universe multiverse
-		deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-security main restricted universe multiverse
-		
-		deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-updates main restricted universe multiverse
-		deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-updates main restricted universe multiverse
-		
-		deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-backports main restricted universe multiverse
-		deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-backports main restricted universe multiverse
-		
-		## Not recommended
-		# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-proposed main restricted universe multiverse
-		# deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-proposed main restricted universe multiverse 
+        #如果遇到无法拉取 https 源的情况，请先使用 http 源并安装：
+        #$ sudo apt install apt-transport-https
+        	
+        deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename main restricted universe multiverse
+        deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename main restricted universe multiverse
+
+        deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-security main restricted universe multiverse
+        deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-security main restricted universe multiverse
+	
+        deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-updates main restricted universe multiverse
+        deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-updates main restricted universe multiverse
+ 
+        deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-backports main restricted universe multiverse
+        deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-backports main restricted universe multiverse
+	
+        ## Not recommended
+        # deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-proposed main restricted universe multiverse
+        # deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-proposed main restricted universe multiverse 
 	EOF
   fi	
 fi
