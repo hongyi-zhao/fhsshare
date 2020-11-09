4614dbf0d0ff0a72d7889875f5f0dffbd03fd558
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-11-09 23:33:43 +0800

    modified:   etc/profile.d/100-sources.list.sh

--- etc/profile.d/100-sources.list.sh
+++ etc/profile.d/100-sources.list.sh
@@ -13,17 +13,20 @@ if [ ! -e $sources_list ] || ! grep -q -m1 '^deb https://mirrors.tuna.tsinghua.e
         |#如果遇到无法拉取 https 源的情况，请先使用 http 源并安装：
         |#$ sudo apt install apt-transport-https
         |
+        |# For solving the unmet dependencies of packages, the updates and security components can be used 
+        |# temporarily.
+        |
         |deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename main contrib non-free
         |deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename main contrib non-free
         |
         |#deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-updates main contrib non-free
         |#deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-updates main contrib non-free
+        |      
+        |#deb https://mirrors.tuna.tsinghua.edu.cn/debian-security $codename/updates main contrib non-free
+        |#deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security $codename/updates main contrib non-free
         |
         |#deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-backports main contrib non-free
         |#deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-backports main contrib non-free
-        |
-        |#deb https://mirrors.tuna.tsinghua.edu.cn/debian-security $codename/updates main contrib non-free
-        |#deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security $codename/updates main contrib non-free
 	EOF
   elif lsb_release -i | grep -qi Ubuntu; then
     sed 's/^ *|//' <<-EOF | sudo tee $sources_list >/dev/null
@@ -32,15 +35,18 @@ if [ ! -e $sources_list ] || ! grep -q -m1 '^deb https://mirrors.tuna.tsinghua.e
         | 
         |#如果遇到无法拉取 https 源的情况，请先使用 http 源并安装：
         |#$ sudo apt install apt-transport-https
-        |	
+        |
+        |# For solving the unmet dependencies of packages, the updates and security components can be used 
+        |# temporarily.
+        |        	
         |deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename main restricted universe multiverse
         |deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename main restricted universe multiverse
-        | 
-        |#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-security main restricted universe multiverse
-        |#deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-security main restricted universe multiverse
         |
         |#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-updates main restricted universe multiverse
         |#deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-updates main restricted universe multiverse
+        |         
+        |#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-security main restricted universe multiverse
+        |#deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-security main restricted universe multiverse
         |
         |#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-backports main restricted universe multiverse
         |#deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-backports main restricted universe multiverse
