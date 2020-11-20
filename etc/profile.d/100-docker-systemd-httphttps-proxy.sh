73d09712980551cc3680884de12ce53010cc82af
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-11-20 20:49:42 +0800

    modified:   etc/profile.d/100-docker-systemd-httphttps-proxy.sh

--- etc/profile.d/100-docker-systemd-httphttps-proxy.sh
+++ etc/profile.d/100-docker-systemd-httphttps-proxy.sh
@@ -140,33 +140,6 @@
 #The proxy set in this file is for dockerd which can use any of the host network interfaces, including docker0.
 #The proxy types can be orgnized including http/https/socks5 and socks5h is not supported yet. 
 
-#https://github.com/yeasy/docker_practice/blob/master/install/mirror.md#ubuntu-1604debian-8centos-7
-#Ubuntu 16.04+、Debian 8+、CentOS 7+
-
-#目前主流 Linux 发行版均已使用 systemd 进行服务管理，这里介绍如何在使用 systemd 的 Linux 发行版中配置镜像加速器。
-
-#请首先执行以下命令，查看是否在 docker.service 文件中配置过镜像地址。
-
-#$ systemctl cat docker | grep '\-\-registry\-mirror'
-
-#如果该命令有输出，那么请执行 $ systemctl cat docker 查看 ExecStart= 出现的位置，修改对应的文件内容去掉 --registry-mirror 参数及其值，并按接下来的步骤进行配置。
-
-#如果以上命令没有任何输出，那么就可以在 /etc/docker/daemon.json 中写入如下内容（如果文件不存在请新建该文件）：
-
-#{
-#  "registry-mirrors": [
-#    "https://hub-mirror.c.163.com",
-#    "https://mirror.baidubce.com"
-#  ]
-#}
-
-#    注意，一定要保证该文件符合 json 规范，否则 Docker 将不能启动。
-
-#之后重新启动服务。
-
-#$ sudo systemctl daemon-reload
-#$ sudo systemctl restart docker
-
 #https://github.com/docker-practice/docker-registry-cn-mirror-test/actions/runs/148084125/workflow
 #          registrys="
 #          docker.io
