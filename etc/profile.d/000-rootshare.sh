249afd7bc31768ca0ca7ec4fc0711f525ff899f5
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-12-11 08:48:02 -0500

    modified:   etc/profile.d/000-rootshare.sh

--- etc/profile.d/000-rootshare.sh
+++ etc/profile.d/000-rootshare.sh
@@ -105,7 +105,7 @@ fi
 
 codename=$(lsb_release -sc)
 sources_list=/etc/apt/sources.list
-
+MIRROR=mirrors.tuna.tsinghua.edu.cn
 
 #info sed
 #'[:blank:]'
@@ -114,8 +114,10 @@ sources_list=/etc/apt/sources.list
 #     Space characters: in the 'C' locale, this is tab, newline, vertical
 #     tab, form feed, carriage return, and space.
 
-if lsb_release -i | grep -qi Debian; then
-  sed 's/^[[:blank:]]*[|]//' <<-EOF | sudo tee $sources_list >/dev/null
+
+if ! grep -qE "^[[:blank:]]*deb(-src)? https?://$MIRROR/" $sources_list; then
+  if lsb_release -i | grep -qi Debian; then
+    sed 's/^[[:blank:]]*[|]//' <<-EOF | sudo tee $sources_list >/dev/null
         |#http://mirrors.ustc.edu.cn/repogen/
         |#https://mirrors.tuna.tsinghua.edu.cn/help/debian/
         |
@@ -125,20 +127,20 @@ if lsb_release -i | grep -qi Debian; then
         |# For solving the unmet dependencies of packages, the updates and security suites can be used 
         |# temporarily.
         |
-        |deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename main contrib non-free
-        |deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename main contrib non-free
+        |deb https://$MIRROR/debian/ $codename main contrib non-free
+        |deb-src https://$MIRROR/debian/ $codename main contrib non-free
         |
-        |deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-updates main contrib non-free
-        |deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-updates main contrib non-free
+        |deb https://$MIRROR/debian/ $codename-updates main contrib non-free
+        |deb-src https://$MIRROR/debian/ $codename-updates main contrib non-free
         |      
-        |deb https://mirrors.tuna.tsinghua.edu.cn/debian-security $codename/updates main contrib non-free
-        |deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security $codename/updates main contrib non-free
+        |deb https://$MIRROR/debian-security $codename/updates main contrib non-free
+        |deb-src https://$MIRROR/debian-security $codename/updates main contrib non-free
         |
-        |#deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-backports main contrib non-free
-        |#deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename-backports main contrib non-free
+        |#deb https://$MIRROR/debian/ $codename-backports main contrib non-free
+        |#deb-src https://$MIRROR/debian/ $codename-backports main contrib non-free
 	EOF
-elif lsb_release -i | grep -qi Ubuntu; then
-  sed 's/^[[:blank:]]*[|]//' <<-EOF | sudo tee $sources_list >/dev/null
+  elif lsb_release -i | grep -qi Ubuntu; then
+    sed 's/^[[:blank:]]*[|]//' <<-EOF | sudo tee $sources_list >/dev/null
         |#http://mirrors.ustc.edu.cn/repogen/
         |#https://mirrors.tuna.tsinghua.edu.cn/help/debian/
         | 
@@ -148,39 +150,38 @@ elif lsb_release -i | grep -qi Ubuntu; then
         |# For solving the unmet dependencies of packages, the updates and security suites can be used 
         |# temporarily.
         |        	
-        |deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename main restricted universe multiverse
-        |deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename main restricted universe multiverse
+        |deb http://$MIRROR/ubuntu/ $codename main restricted universe multiverse
+        |deb-src http://$MIRROR/ubuntu/ $codename main restricted universe multiverse
         |
-        |deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-updates main restricted universe multiverse
-        |deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-updates main restricted universe multiverse
+        |deb http://$MIRROR/ubuntu/ $codename-updates main restricted universe multiverse
+        |deb-src http://$MIRROR/ubuntu/ $codename-updates main restricted universe multiverse
         |         
-        |deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-security main restricted universe multiverse
-        |deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-security main restricted universe multiverse
+        |deb http://$MIRROR/ubuntu/ $codename-security main restricted universe multiverse
+        |deb-src http://$MIRROR/ubuntu/ $codename-security main restricted universe multiverse
         |
-        |#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-backports main restricted universe multiverse
-        |#deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-backports main restricted universe multiverse
+        |#deb http://$MIRROR/ubuntu/ $codename-backports main restricted universe multiverse
+        |#deb-src http://$MIRROR/ubuntu/ $codename-backports main restricted universe multiverse
         |
         |## Not recommended
-        |# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-proposed main restricted universe multiverse
-        |# deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename-proposed main restricted universe multiverse 
+        |# deb http://$MIRROR/ubuntu/ $codename-proposed main restricted universe multiverse
+        |# deb-src http://$MIRROR/ubuntu/ $codename-proposed main restricted universe multiverse 
 	EOF
+  fi
+
+  # man apt-get
+  #       -q, --quiet
+  #           Quiet; produces output suitable for logging, omitting progress
+  #           indicators. More q's will produce more quiet up to a maximum of 2.
+  #           You can also use -q=# to set the quiet level, overriding the
+  #           configuration file. Note that quiet level 2 implies -y; you should
+  #           never use -qq without a no-action modifier such as -d, --print-uris
+  #           or -s as APT may decide to do something you did not expect.
+  #           Configuration Item: quiet.
+
+  # Some needed tools:
+  sudo apt-get -qq install dialog apt-transport-https apt-utils ca-certificates software-properties-common gnupg-agent devscripts gcc make cmake git netcat ncat socat tcpdump pv iftop haproxy gawk uuid gedit-plugins gnome-tweak-tool telegram-desktop curl apt-file gparted gddrescue partclone gdebi zstd jq tor golang synaptic gimp pandoc equivs unrar
+  sudo apt-get -qq build-dep golang
 fi	
-
-# man apt-get
-#       -q, --quiet
-#           Quiet; produces output suitable for logging, omitting progress
-#           indicators. More q's will produce more quiet up to a maximum of 2.
-#           You can also use -q=# to set the quiet level, overriding the
-#           configuration file. Note that quiet level 2 implies -y; you should
-#           never use -qq without a no-action modifier such as -d, --print-uris
-#           or -s as APT may decide to do something you did not expect.
-#           Configuration Item: quiet.
-
-
-# Some needed tools:
-sudo apt-get -qq install dialog apt-transport-https apt-utils ca-certificates software-properties-common gnupg-agent devscripts gcc make cmake git netcat ncat socat tcpdump pv iftop haproxy gawk uuid gedit-plugins gnome-tweak-tool telegram-desktop curl apt-file gparted gddrescue partclone gdebi zstd jq tor golang synaptic gimp pandoc equivs unrar
-sudo apt-get -qq build-dep golang
-
 
 
 #For Apollo D-kit hardware platform.
