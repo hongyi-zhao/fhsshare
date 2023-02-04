ec8e8b53cf3733e98f7744330d31f1fd9c014b0e
Hongyi Zhao <hongyi.zhao@gmail.com>
2023-02-04 10:56:13 +0800

    modified:   etc/profile.d/000-git-env.sh

--- etc/profile.d/000-git-env.sh
+++ etc/profile.d/000-git-env.sh
@@ -8,6 +8,22 @@
 #           boundaries. Like GIT_CEILING_DIRECTORIES, this will not affect an explicit repository directory set via
 #           GIT_DIR or on the command line.
 
+
+#https://discourse.gnome.org/t/are-you-sure-you-want-to-proceed-message-on-login/13758/8?u=hongyi-zhao
+#$ sudo gedit /etc/gdm3/config-error-dialog.sh
+
+#elif [ -x /usr/bin/zenity ]; then
+#        # https://discourse.gnome.org/t/are-you-sure-you-want-to-proceed-message-on-login/13758/5?u=hongyi-zhao
+#	zenity --no-markup --warning --no-wrap --text="$TEXT"
+#fi
+
+
+#Got it. In my case, I need to enable the following settings in my profile related scripts to let them work properly:
+
+#git config --global --add safe.directory /
+#export GIT_DISCOVERY_ACROSS_FILESYSTEM=true
+#export GIT_SSL_NO_VERIFY=1
+
 export GIT_DISCOVERY_ACROSS_FILESYSTEM=true
 export GIT_SSL_NO_VERIFY=1
 
